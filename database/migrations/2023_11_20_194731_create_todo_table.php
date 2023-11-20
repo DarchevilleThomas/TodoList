9d98a493b8233122df15ddf20125853567a50e58
Thomas Darcheville <darcheville.toma@gmail.com>
2023-11-20 20:53:08 +0100

    MAJ de la migration 'todo'

--- database/migrations/2023_11_20_194731_create_todo_table.php
+++ database/migrations/2023_11_20_194731_create_todo_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('todo', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('tache');
+            $table->string('description');
+            $table->boolean('fini');
+            $table->dateTime('dateDeFin');
         });
     }
 
