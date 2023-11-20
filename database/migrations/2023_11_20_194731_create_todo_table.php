26948544553dd4ad921f20a5911590deb5c3e825
Thomas Darcheville <darcheville.toma@gmail.com>
2023-11-20 21:03:29 +0100

    Creation du controller TodoController

--- database/migrations/2023_11_20_194731_create_todo_table.php
+++ database/migrations/2023_11_20_194731_create_todo_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('todo', function (Blueprint $table) {
             $table->id();
             $table->string('tache');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->boolean('fini');
             $table->dateTime('dateDeFin');
         });
