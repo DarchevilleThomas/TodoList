01865d5de8ae6deeceed321de5459c7b3ebe729e
Thomas Darcheville <darcheville.toma@gmail.com>
2023-11-22 19:51:05 +0100

    Ajout de bootstrap dans la vue todolist

--- resources/views/todolist.blade.php
+++ resources/views/todolist.blade.php
@@ -18,17 +18,22 @@
             height: 50px;
         }
     </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
 
+<div class="text-center">
 <h1>Liste de tâches</h1>
 
 @if (session('status'))
     <div> {{session('status')}}</div>
 @endif
-<a href="/ajouter"><h2>Ajout de tâche</h2></a>
-<a href="/delete"><h2>Tout supprimer</h2></a>
+<div>
 
+<a href="/ajouter" class="btn btn-success btn-sm me-3"><h3>Ajout de tâche</h3></a>
+<a href="/delete" class="btn btn-danger btn-sm"><h3>Tout supprimer</h3></a>
+</div>
+</div>
 @if(sizeof($todos)>0)
 <table>
     <thead>
@@ -50,7 +55,7 @@
         @else
             <td><a href="/activate/{{$todo['id']}}"><div class="true"></div></a></td>
         @endif
-        <td><a href="/edit/{{$todo['id']}}">Editer</a></td>
+        <td><a href="/edit/{{$todo['id']}}" class="btn btn-primary">Editer</a></td>
     </tr>
     @endforeach
     </tbody>
